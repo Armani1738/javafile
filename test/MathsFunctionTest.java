7ede4aa1bf24f23786d2731384499f7c9d531c18
Armani <primearmani@gmail.com>
2023-09-20 06:25:04 +0100

    commit

--- test/MathsFunctionTest.java
+++ test/MathsFunctionTest.java
@@ -5,45 +5,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathsFunctionTest {
     @Test
-    public void testEvenOdd(){
-        boolean number = MathsFunction.evenOdd(4);
-        assertEquals(true,number);
+    public void testEvenOdd() {
+        boolean isEven = MathsFunction.evenOdd(4);
+        assertEquals(true, isEven);
     }
+
     @Test
-    public void testPrimeNumbers(){
-        boolean number = MathsFunction.primeNumber(18);
-        assertEquals(false, number);
+    public void testPrimeNumbers() {
+        boolean isPrime = MathsFunction.primeNumber(18);
+        assertEquals(false, isPrime);
     }
+
     @Test
-    public  void testSubtract(){
-        int numbers = MathsFunction.subtract(8, 15);
-        assertEquals(7, numbers);
+    public void testSubtract() {
+        int subtract = MathsFunction.subtract(8, 15);
+        assertEquals(7, subtract);
     }
+
     @Test
-    public void testDivide(){
-        double divide = MathsFunction.divide(10,2);
+    public void testDivide() {
+        double divide = MathsFunction.divide(10, 2);
         assertEquals(5, divide);
     }
+
     @Test
-    public void testFactorial(){
-        int factorial = MathsFunction.factor(15);
-        assertEquals(4, factorial);
+    public void testFactorial() {
+        int factorOf = MathsFunction.factor(15);
+        assertEquals(4, factorOf);
 
     }
+
     @Test
-    public void testsquare(){
-        boolean square = MathsFunction.square(25);
-        assertEquals(true, square);
+    public void testsquare() {
+        boolean isSquare = MathsFunction.square(25);
+        assertEquals(true, isSquare);
     }
+
     @Test
-    public void palindrome(){
-        boolean palindrome = MathsFunction.palindrome(5);
-        assertEquals(true, palindrome);
+    public void palindrome() {
+        boolean isPalindrome = MathsFunction.palindrome(5);
+        assertEquals(true, isPalindrome);
     }
+
     @Test
-    public void factorial(){
-        int factorial = MathsFunction.factorial(15);
-        assertEquals(2004310016, factorial);
+    public void factorial() {
+        int factorialOf = MathsFunction.factorial(15);
+        assertEquals(2004310016, factorialOf);
     }
 
 }
