408810a82c1bb40cf9f76306d380bfeb7239cf47
Armani <primearmani@gmail.com>
2023-10-01 15:03:32 +0100

    java

--- src/ChibuzorArrayAssignment/ArrayFunctions.java
+++ src/ChibuzorArrayAssignment/ArrayFunctions.java
@@ -1,6 +1,12 @@
 package ChibuzorArrayAssignment;
 
+import java.util.Arrays;
+
 public class ArrayFunctions {
+    public static void main(String[] args) {
+        int[] integer = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(Arrays.toString(odd(integer)));
+    }
     public static int largest(int[] score) {
 //        int[] figure = {2,4,5,9,3};
         int largest = 0;
@@ -21,5 +27,16 @@ public class ArrayFunctions {
         }
         return reverse;
     }
+    public static int[] odd(int[] Number){
+        int []odd = new int [Number.length/2];
+        int oddNumber = 0;
+        for (int count = 1; count <= Number.length ; count+=2) {
+            odd[oddNumber] = Number[count];
+                oddNumber++;
+            }
+        return odd;
+        }
 
-}
+    }
+
+
--- test/ArrayFunctionsTest.java
+++ test/ArrayFunctionsTest.java
@@ -30,7 +30,10 @@ public class ArrayFunctionsTest {
     }
     @Test
     void testArraylist(){
-
+        int[] number = {1,2,3,4,5,6,7,8,9,10};
+        int[] result = {2,4,6,8,10};
+        int[] value = ArrayFunctions.odd(number);
+        assertArrayEquals(result, value);
     }
 
 }
